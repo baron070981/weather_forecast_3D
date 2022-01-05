de219308276451de2355e8e3997ab6d9f0005c23
baron070981 <baron070981@gmail.com>
2022-01-05 23:57:29 +0300

    добавил класс Weather

--- weather/weather.py
+++ weather/weather.py
@@ -8,15 +8,19 @@ import os.path
 import time
 import json
 import rich
+import time
+from pathlib import Path
 
-
-import weatherdata
-import secret
+try:
+    import weatherdata
+    import secret
+except:
+    from . import weatherdata
+    from . import secret
 
 console = console.Console()
 
-
-data_file = './config.json'
+data_file = Path(__file__).resolve().parent.joinpath('config.json')
 GETDATA = False
 tmp = {}
 LAT = 63.00
@@ -77,39 +81,176 @@ def get_angle_sun_at_noon(latitude):
 
 def get_sun_path_lenght(lenght_day):
     '''
-    lenght_day - int, продолжительность дня в минутах
-    '''
-    noon = int(lenght_day/2)
+    расчитывается длина солнечного пути
     
+    lenght_day - int, продолжительность дня в минутах 
+    '''
+    ...
+    
+
+def get_sun_position(sunrise, sunset, current_time=None):
+    '''
+    
+    производится расчет положения солнца в определенный момент времени
+    
+    sunrise - time.struct_time
+    sunset - time.struct_time
+    current_time = time.struct_time
+    '''
+    curtime = current_time if current_time is not None else time.gmtime(time.time())
+    hour = curtime.tm_hour
+    minutes = curtime.tm_min
+
+
+def get_season(current_date=None):
+    '''
+    current_date - time.struct_time или str или tuple
+    строка должна иметь такой формат DD.MM (01.07 - первое июля)
+    кортеж сотоит из двух элементов int - (число, месяц)
+    
+    '''
+    month = None
+    day = None
+    
+    if current_date is not None:
+        if isinstance(current_date, time.struct_time):
+            day = current_date.tm_mday
+            month = current_date.tm_mon
+        elif isinstance(current_date, str):
+            day, month = list(map(int, current_date.split('.')))
+        elif isinstance(current_date, tuple) or isinstance(current_date, list):
+            day, month = list(map(int, current_date))
+        else:
+            raise TypeError('Не верный тип. Требуется time.struct_time, tuple, list, str')
+    else:
+        current_date = time.gmtime(time.time())
+        day = current_date.tm_mday
+        month = current_date.tm_mon
+    
+    if month == 12 or month == 1 or month == 2:
+        # зима
+        return 0
+    elif month == 3 or (month == 4 and day <= 23):
+        # первая часть весны, со снегом
+        return 1
+    elif (month == 4 and day > 23) or month == 5:
+        # вторая часть весны, без снега, молодая листва
+        return 2
+    elif month > 5 and month < 9:
+        # лето
+        return 3
+    elif month == 9 or (month == 10 and day < 16):
+        # золотая осень
+        return 4
+    elif (month == 10 and day >= 16) or month == 11:
+        # осень
+        return 5
+    raise Exception('Передана неверная дата.')
+
+
+
+
+
+
+class Weather:
+    
+    # путь к файлу в котором хранятся настройки и данные погоды
+    data_file = Path(__file__).resolve().parent.joinpath('config.json')
+    
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.DATASTATE = False # переменная определяет нужно ли получать новые данные погоды
+        self.__create_conf()  # создание config.json если он не существует
+        self.data = self.load_weatherdata()
+        self.old_time = self.data['CONF']['TIME']
+        self.interval = self.data['CONF']['INTERVAL']
+        self.lat = self.data['CONF']['LAT']
+        self.lon = self.data['CONF']['LON']
+        
+        if self.__check_time():
+            self.DATASTATE = True
+        
+        if self.DATASTATE:
+            print('Получение новых погодных данных')
+            tmp_data = self.get_data()
+            weatherdata.update_data(tmp_data)
+            
+            conf = {'TIME':time.time(), 
+                    'LAT':self.lat, 
+                    'LON':self.lon, 
+                    'INTERVAL':self.interval}
+            weatherdata.update_conf(conf)
+            save_data(data_file, weatherdata.confdata)
+            self.data = self.load_weatherdata()
+    
+    
+    
+    def __create_conf(self):
+        '''
+        проверка существования файла
+        если не существует, то создается с дефолтными значениями
+        '''
+        if not os.path.isfile(Weather.data_file):
+            _data = weatherdata.default_data.copy()
+            with open(filename, 'w') as f:
+                json.dump(_data, f)
+                print('Создание файла config.json')
+            seld.DATASTATE = True
+    
+    def load_weatherdata(self):
+        data = None
+        with open(self.data_file, 'r') as f:
+            data = json.load(f)
+        return data
+    
+    def __check_time(self):
+        return (time.time() - self.old_time) >= self.interval
+    
+    def get_data(self):
+        resp = yandex.get(
+                    requests, 
+                    api_key=self.api_key, 
+                    rate='informers', 
+                    lat=self.lat, 
+                    lon=self.lon
+                )
+        rich.print(resp)
+        data = weatherdata.format_data(dict(resp['fact']))
+        return data
 
 
 
 if __name__ == '__main__':
     
-    if not is_config(data_file):
-        save_data(data_file)
-        LAT = weatherdata.default_data['CONF']['LAT']
-        LON = weatherdata.default_data['CONF']['LON']
-        interval = weatherdata.default_data['CONF']['INTERVAL']
-        GETDATA = True
-    else:
-        tmp = load_weatherdata(data_file)
-        print('Load data:', tmp)
-        old_time = tmp['CONF']['TIME']
-        interval = tmp['CONF']['INTERVAL']
-        LAT = weatherdata.default_data['CONF']['LAT']
-        LON = weatherdata.default_data['CONF']['LON']
-        if check_time(old_time, now_time, interval):
-            GETDATA = True
+    get_season()
+    
+    w = Weather(secret.token)
+    rich.print(w.data)
+    
+    # if not is_config(data_file):
+        # save_data(data_file)
+        # LAT = weatherdata.default_data['CONF']['LAT']
+        # LON = weatherdata.default_data['CONF']['LON']
+        # interval = weatherdata.default_data['CONF']['INTERVAL']
+        # GETDATA = True
+    # else:
+        # tmp = load_weatherdata(data_file)
+        # print('Load data:', tmp)
+        # old_time = tmp['CONF']['TIME']
+        # interval = tmp['CONF']['INTERVAL']
+        # LAT = weatherdata.default_data['CONF']['LAT']
+        # LON = weatherdata.default_data['CONF']['LON']
+        # if check_time(old_time, now_time, interval):
+            # GETDATA = True
     
     
-    if GETDATA:
-        tmp = get_data(secret.token, str(LAT), str(LON))
-        tmp = weatherdata.update_data(tmp)
-        conf = {'TIME':now_time, 'LAT':LAT, 'LON':LON, 'INTERVAL':interval}
-        conf = weatherdata.update_conf(conf)
-        save_data(data_file, weatherdata.confdata)
-        print('Getting data:', weatherdata.confdata)
+    # if GETDATA:
+        # tmp = get_data(secret.token, str(LAT), str(LON))
+        # tmp = weatherdata.update_data(tmp)
+        # conf = {'TIME':now_time, 'LAT':LAT, 'LON':LON, 'INTERVAL':interval}
+        # conf = weatherdata.update_conf(conf)
+        # save_data(data_file, weatherdata.confdata)
+        # print('Getting data:', weatherdata.confdata)
 
 
 
