466983a6bce5135a21b9ca97e47545153b7fb7df
baron070981 <baron070981@gmail.com>
2022-01-05 23:47:20 +0300

    загрузка моделей в зависимости от времени года

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from ogl import mainwindow
-from weather import *
+from weather import weather, secret
+from ogl.mainwindow import Models
 
+from rich import console
+
+cons = console.Console()
 
 # ==================================
 
@@ -8,22 +12,23 @@ mainwindow.set_window_size()
 
 # ==================================
 
+season = weather.get_season()
+weather_data = weather.Weather(secret.token).data['DATA']
+cons.print(weather_data)
 
 
+models = Models()
 
+if season == 0:
+    models.landscape = 'landscape_winter.glb'
+    models.house = 'wooden_house_wint.glb'
 
 
 # ==================================
 
 if __name__ == '__main__':
     
-    
-    
-    
-    
-    
-    world = mainwindow.World()
+    world = mainwindow.World(models)
     world.run()
-
 
 
